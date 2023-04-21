182e28a624936a161ba6cde92bc53d7fa7394abb
Sharon Arana <shar_mdza@hotmail.com>
2023-04-21 16:51:31 -0600

    Configuración inicial del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { UseState } from "./UseState";
+import { ClassState } from "./ClassState";
+
+function App() {
+  return (
+    <div className="App">
+      <UseState />
+      <ClassState />
+    </div>
+  );
+}
+
+export default App;
