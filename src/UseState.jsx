8cd054d2d77093a9b78175bfd05e2e5cfddb441c
Sharon Arana <shar_mdza@hotmail.com>
2023-04-24 08:35:15 -0600

    Uso de useEffect en UseState

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -2,13 +2,34 @@ import React from "react";
 
 function UseState({ name }) {
   const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    console.log("Iniciando el efecto");
+
+    if (loading) {
+      setTimeout(() => {
+        console.log("Haciendo la validación");
+
+        setLoading(false);
+
+        console.log("Terminando la validación");
+      }, 3000);
+    }
+
+    console.log("Terminando el efecto");
+  }, [loading]);
+
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>Por favor escribe el código de seguridad</p>
+
       {error && <p>Error: El codigo es incorrecto</p>}
+      {loading && <p>Cargando ...</p>}
+
       <input placeholder="Código de seguridad" />
-      <button onClick={() => setError(!error)}>Comprobar</button>
+      <button onClick={() => setLoading(true)}>Comprobar</button>
     </div>
   );
 }
