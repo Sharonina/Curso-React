cb0017e11aa184e5d569c46b93616fc4861f881e
Sharon Arana <shar_mdza@hotmail.com>
2023-04-24 08:55:47 -0600

    Métodos del ciclo de vida

--- src/ClassState.jsx
+++ src/ClassState.jsx
@@ -6,18 +6,35 @@ class ClassState extends React.Component {
     super(props);
     this.state = {
       error: false,
+      loading: false,
     };
   }
+
+  // métodos del ciclo de vida
+  //componentWillMount
+  UNSAFE_componentWillMount() {
+    console.log("componentWillMount");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
   render() {
     return (
       <div>
         <h2>Eliminar {this.props.name}</h2>
         <p>Por favor escribe el código de seguridad</p>
         {this.state.error && <p>Error: El codigo es incorrecto</p>}
+        {this.state.loading && <p>Cargando...</p>}
         <input placeholder="Código de seguridad" />
         {/* this.setState((prevState) => ({ error: !prevState }))}
         > */}
-        <button onClick={() => this.setState({ error: !this.state.error })}>
+        <button onClick={() => this.setState({ loading: true })}>
           Comprobar
         </button>
       </div>
