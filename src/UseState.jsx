00d3dd3775ce8579f6afaaa0e31d6e11a7d24be5
Sharon Arana <shar_mdza@hotmail.com>
2023-04-24 10:34:09 -0600

    validación de estados independientes y compuestos

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 
 function UseState({ name }) {
+  const [value, setValue] = React.useState("");
   const [error, setError] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
+
+  const SECURITY_CODE = "paradigma";
 
   React.useEffect(() => {
     console.log("Iniciando el efecto");
 
     if (loading) {
+      //setError(false);
       setTimeout(() => {
         console.log("Haciendo la validación");
 
+        if (value !== SECURITY_CODE) {
+          setError(true);
+        }
         setLoading(false);
 
         console.log("Terminando la validación");
@@ -20,15 +27,20 @@ function UseState({ name }) {
     console.log("Terminando el efecto");
   }, [loading]);
 
+  console.log(value);
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>Por favor escribe el código de seguridad</p>
 
-      {error && <p>Error: El codigo es incorrecto</p>}
+      {error && !loading && <p>Error: El codigo es incorrecto</p>}
       {loading && <p>Cargando ...</p>}
 
-      <input placeholder="Código de seguridad" />
+      <input
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+        placeholder="Código de seguridad"
+      />
       <button onClick={() => setLoading(true)}>Comprobar</button>
     </div>
   );
